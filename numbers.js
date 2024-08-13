4e01706175322dc3cbdfe5f2d01791c6662bef59
Marlon <mgflores012012@gmail.com>
2024-08-12 20:02:51 -0600

    Added numbers, data-types and comparation-types

---
+++ numbers.js
@@ -0,0 +1,36 @@
+let manzanas = 4
+let libraPapa = 1.80
+
+console.log(manzanas)
+console.log(libraPapa)
+
+console.log(typeof manzanas)
+
+//Notacion cientifica
+let atomosAgua = 2.5e8
+
+//Infinito
+let miAmorPorElla = Infinity
+
+//No es un numero
+let noEsNumero = NaN
+
+//Opeardores aritmeticos
+let suma = 20 + 35
+console.log(`El resultado de la suma es: ${suma}`)
+
+let resta = 20 + 9
+console.log(`El resultado de la resta es: ${resta}`)
+
+let multiplicacion = 20 * 35
+console.log(`El resultado de la suma es: ${multiplicacion}`)
+
+//Raiz cuadrada
+let raizCuadrada = Math.sqrt(16)
+
+//Valor absoluto
+let absoluto = Math.abs(-10)
+
+//Aleatorio
+let random = Math.random()
+
