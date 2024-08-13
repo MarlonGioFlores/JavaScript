2aa83ebc57c36a6b801949752abcd9729c108ca5
Marlon <mgflores012012@gmail.com>
2024-08-12 19:46:41 -0600

    added string js

---
+++ strings.js
@@ -0,0 +1,15 @@
+let fullname = 'Fernando'
+let nationality = 'Salvadorian'
+
+console.log(fullname)
+console.log(nationality)
+
+console.log('Hello ' + fullname + ' Good Morning')
+
+//concatenacion de forma profesional
+console.log(`Hello ${fullname} good morning`)
+
+let phrase = 'Camaron que se duerme se lo lleva la corriente'
+
+console.log(phrase.length)
+console.log(phrase.toLocaleUpperCase())
