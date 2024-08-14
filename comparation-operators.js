ec93a650f0e27a52e53ac70715777e01df859485
Marlon <mgflores012012@gmail.com>
2024-08-13 18:23:00 -0600

    Comparation operators

---
+++ comparation-operators.js
@@ -0,0 +1,19 @@
+let a = 10
+let b = 5 
+
+//Compara solo valores
+a == b
+
+//Compara valores y tipo de dato 
+a === b
+
+console.log(a == b)
+console.log(a === b)
+
+console.log(a != b)
+
+console.log(a > b)
+console.log(a >= b)
+
+console.log(a < b)
+console.log(a <= b)
