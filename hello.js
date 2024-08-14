13d28785c1778bb71e0d9a924244b95f04942cbd
Marlon <mgflores012012@gmail.com>
2024-08-13 19:43:09 -0600

    Added challenge

--- hello.js
+++ hello.js
@@ -1,16 +1,47 @@
-let secretNumber = Math.floor(Math.random *10+1)
+/* let secretNumber = Math.floor(Math.random() *10+1)
 
 let number = prompt('Adivina el numero secreto entre (0 y 10)')
-console.log(number)
+console.log(number) */
 
 //Si el numero secreto es igual al numero que ingresaste Ganaste
 //Si el numero secreto es mayor al que ingresaste Perdiste
 //Si el numero secreto es menor al que ingresaste perdiste
 
-if(number == secretNumber){
+/* if(number == secretNumber){
     console.log('GANASTE')
 } else if (number <= secretNumber) {
     console.log("ES MENOR AL NUMERO SECRETO")
 } else {
     console.log("ES MAYOR AL NUMERO SECRETO")
+} */
+
+   /*  let curso = prompt("Selecciona un curso (HTML, CSS, JAVASCRIPT):").toUpperCase();
+
+// Validar la entrada
+if (curso === "HTML" || curso === "CSS" || curso === "JAVASCRIPT") {
+    alert("Has seleccionado el curso: " + curso);
+} else {
+    alert("Entrada no válida. Por favor, selecciona un curso válido.");
+    solicitarCurso(); // Volver a solicitar la entrada
 }
+ */
+let curso = prompt("Selecciona un curso:\n1. HTML\n2. CSS\n3. JAVASCRIPT");
+
+            // Convertir la entrada a número entero
+            curso = parseInt(curso, 10);
+
+            // Validar la entrada
+            switch (curso) {
+                case 1:
+                    alert("Has seleccionado el curso: HTML");
+                    break;
+                case 2:
+                    alert("Has seleccionado el curso: CSS");
+                    break;
+                case 3:
+                    alert("Has seleccionado el curso: JAVASCRIPT");
+                    break;
+                default:
+                    alert("Entrada no válida. Por favor, selecciona un número entre 1 y 3.");
+                    solicitarCurso(); // Volver a solicitar la entrada
+            }
--- switch.js
+++ switch.js
@@ -2,12 +2,12 @@ let paymentMethod = 'cash'
 
 switch (paymentMethod){
     case 'cash':
-        console.log('Your pay with cash')
+        console.log('You payed with cash')
         break
     case 'credit card':
-        console.log('you pay with credit card')
+        console.log('you payed with credit card')
     case 'cacao':
-    console.log('you pay with cacao')
+    console.log('you payed with cacao')
     break
     default:
         console.log('Invalid payment method')
